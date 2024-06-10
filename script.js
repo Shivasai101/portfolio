3b798d443551e7318785b8f130976377d84c92d8
pavankadavath <144763372+pavankadavath@users.noreply.github.com>
2024-06-10 14:13:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('mail').addEventListener('click', function() {
+    window.location.href = 'mailto:kadavathpavan12345@gmail.com';
+});
+
+// Event listener for clicking on LinkedIn
+document.getElementById('linkedin').addEventListener('click', function() {
+    window.open('https://www.linkedin.com/in/pavan-kadavath-16b515221/');
+});
